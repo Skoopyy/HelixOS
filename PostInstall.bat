561936b8e95acac088d3476ae7fe89288ed5da4d
Skoopyy <104071935+Skoopyy@users.noreply.github.com>
2023-05-11 20:50:05 +0700

    v2.2.8

--- PostInstall.bat
+++ PostInstall.bat
@@ -5,6 +5,8 @@ cd /
 cd HelixOS
 start HelixOSPostInstall.exe
 timeout 10 /nobreak > nul
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\HelixOS\DefaultWallpaper.jpg" /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im HelixOSPostInstall.exe
 taskkill /f /im "HelixOS PostInstall.exe"
 start explorer.exe
