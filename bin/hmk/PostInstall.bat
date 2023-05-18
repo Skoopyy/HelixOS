9638b9766b4d976d59ba13f1805db60106adddc3
Skoopyy <104071935+Skoopyy@users.noreply.github.com>
2023-05-18 18:52:16 +0700

    Removing PostInstall

--- bin/hmk/PostInstall.bat
+++ bin/hmk/PostInstall.bat
@@ -4,8 +4,6 @@ echo HelixOS Post-Install Backend
 cd /
 cd HelixOS
 cd HMK
-start HelixOSPostInstall.exe
-timeout 6 /nobreak > nul
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\HelixOS\HMK\DefaultWallpaper.jpg" /f
 timeout 2 /nobreak > nul
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
